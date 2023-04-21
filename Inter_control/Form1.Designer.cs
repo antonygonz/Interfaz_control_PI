75aebbacd451322456db0c35cc1bdb8fdf455175
Antonio Gonzalez <antonygonz266@gmail.com>
2023-04-21 09:16:46 -0600

    Agregar archivos de proyecto.

---
+++ Inter_control/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Inter_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tbRecibir = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(414, 101);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(66, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // tbRecibir
+            // 
+            this.tbRecibir.Location = new System.Drawing.Point(87, 360);
+            this.tbRecibir.Name = "tbRecibir";
+            this.tbRecibir.Size = new System.Drawing.Size(100, 22);
+            this.tbRecibir.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbRecibir);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox tbRecibir;
+    }
+}
+
---
+++ Inter_control/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Inter_control
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puerto;
+        bool estaconectado;
+        static Series dist; //Serie a graficar
+        public delegate void AñadirDatosDelegate(String str);
+        public AñadirDatosDelegate del;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.del=new AñadirDatosDelegate((string s) => { tbRecibir.AppendText(s); });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            if (!estaconectado)
+            {
+                try
+                {
+                    estaconectado = true;
+                    string puerto_selec = comboBox1.GetItemText(comboBox1.SelectedItem);
+                    puerto = new SerialPort(puerto_selec,9600,Parity.None,8,StopBits.One);
+
+                    puerto.Open();
+                    button1.Text = "Cerrar conexion";
+                    button1.BackColor = Color.Red;
+                    button1.ForeColor=Color.White;
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Error seleccione puerto");
+                }
+            }
+            else
+            {
+                estaconectado = false;
+                puerto.Close();
+                button1.Text = "Abrir Conexion";
+                button1.BackColor = Color.Blue;
+                button1.ForeColor = Color.White;
+
+            }
+        }
+        static string recibirDato;
+        static double datoTemp, convertirNumero = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Conexion Micro";
+            chart1.Titles.Add("lectura distancia Sensor");
+            dist = chart1.Series.Add("Distancia");
+
+            chart1.Series["Distancia"].ChartType = SeriesChartType.Line;
+
+            string[] puertos = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(puertos);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (puerto.IsOpen)
+            {
+                recibirDato = puerto.ReadExisting();
+
+                if (recibirDato != "") ;
+                {
+                    tbRecibir.Text = recibirDato;
+
+                    if (double.TryParse(recibirDato, out convertirNumero))
+                    {
+                        datoTemp = convertirNumero;
+                    }
+                    dist.Points.Add(datoTemp);
+
+                    recibirDato = "";
+                    puerto.DiscardInBuffer();
+                }
+            }
+        }
+    }
+}
